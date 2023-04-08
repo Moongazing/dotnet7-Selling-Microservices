6faef7b4b482f0afe1f844160c6da4c0ea9f8a5d
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-04-08 12:55:00 +0300

    Adding Factory

---
+++ src/BuildingBlocks/EventBus/Moongazing.EventBus.Factory/EventBusFactory.cs
@@ -0,0 +1,26 @@
+﻿using Moongazing.EventBus.Base;
+using Moongazing.EventBus.Base.Abstraction;
+using Moongazing.EventBus.RabbitMQ;
+using Moongazing.EventBus.AzureServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Moongazing.EventBus.Base.EventBusConfig;
+
+namespace Moongazing.EventBus.Factory
+{
+    public static class EventBusFactory
+    {
+        public static IEventBus Create(EventBusConfig config, IServiceProvider serviceProvider)
+        {
+
+            return config.EventBustType switch
+            {
+                EventBusType.AzureServiceBus => new EventBusServiceBus(config, serviceProvider),
+                _ => new EventBusRabbitMQ(config, serviceProvider),
+            };
+        }
+     }
+}
